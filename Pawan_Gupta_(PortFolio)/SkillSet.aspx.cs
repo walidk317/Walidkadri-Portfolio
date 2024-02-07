1586550d83ec775fd4693c831f68c0d98d00ae31
walidk317 <144201622+walidk317@users.noreply.github.com>
2024-02-07 12:32:36 +0530

    Create SkillSet.aspx.cs

---
+++ Pawan_Gupta_(PortFolio)/SkillSet.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Walidkadri_Portfolio
+{
+    public partial class SkillSet : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Download_Click(object sender, EventArgs e)
+        {
+            Response.ContentType = "Application/pdf";
+            Response.AppendHeader("Content-Disposition", "attachment; filename=Walid_CV.pdf");
+            Response.TransmitFile(Server.MapPath("~/Resume/Walid_CV.pdf"));
+            Response.End();
+        }
+    }
+}
